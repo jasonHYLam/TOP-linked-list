33a7c5d042b73c0b144f9c277256e095e904353a
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-10 10:10:22 +0100

    Create Appending Functions And Accessing Functions
    Created append(), prepend(), which add values to the list. Also head()
    and tail() are created which access the first and last element in the
    list respectively. Finally size() is created which returns the length
    of the list.

--- linkedList.js
+++ linkedList.js
@@ -2,6 +2,26 @@ console.log('h')
 
 function createLinkedList() {
     let list = [];
+
+    function append(value) {
+        list.push(value)
+    }
+
+    function prepend(value) {
+        list.unshift(value)
+    }
+
+    function size() {
+        return list.length
+    }
+
+    function head() {
+        return list.find(Boolean) // returns the first element
+    }
+
+    function tail() {
+        return list.slice(-1)[0] // returns the last element
+    }
 }
 
 function createNode() {
