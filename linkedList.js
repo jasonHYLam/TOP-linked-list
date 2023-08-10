32eed2c5e3b1e1ab98d4bb298648ab198c8584ca
prodijay777 <jasonlamdarksouls@gmail.noreply.github.com>
2023-08-10 10:59:16 +0100

    Refactor createNode()
    Refactor such that its value property is assigned the value argument.

--- linkedList.js
+++ linkedList.js
@@ -3,28 +3,64 @@ console.log('h')
 function createLinkedList() {
     let list = [];
 
-    function append(value) {
-        list.push(createNode(value))
+    function append(value) { // possible error here
+        const newNode = createNode(value)
+        console.log(newNode)
+        list.push(newNode)
+        console.log(list)
     }
 
     function prepend(value) {
-        list.unshift(createNode(value))
+        const newNode = createNode(value)
+        list.unshift(newNode)
     }
 
     function size() {
         return list.length
     }
 
+    function testConsoleLog() {
+        console.log(list)
+    }
+
     function head() {
-        return list.find(Boolean) // returns the first element
+        console.log(list)
+        console.log(list.find(Boolean))
+        return list.find(Boolean).getValue() // returns the first element
     }
 
     function tail() {
         return list.slice(-1)[0] // returns the last element
     }
+
+    return {
+        append,
+        prepend,
+        size,
+        head, 
+        tail,
+        testConsoleLog,
+
+    }
 }
 
-function createNode() {
-    let value = null;
+function createNode(value = null) {
+    let nodeValue = value;
     let nextNodeLink = null;
+
+    function getValue() {
+        return nodeValue;
+    }
+
+    return {getValue,}
 }
+
+const newLinkedList = createLinkedList();
+newLinkedList.append(4); // possible error in append()
+newLinkedList.append(10);
+console.log(newLinkedList.head())
+newLinkedList.prepend(3);
+console.log(newLinkedList.head())
+
+createLinkedList().testConsoleLog();
+
